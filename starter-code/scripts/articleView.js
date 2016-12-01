4f48666c6f5ade9c78e97830ec2a90bf11a50dca
sueanyv <valenciasue@hotmail.com>
2016-11-30 16:48:48 -0800

    changes in js

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,7 +40,26 @@ articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+      /* TODO: If the select box changes to an option that has a value, we should:
+          1. Hide all of the articles
+          2. Fade in only the articles that match based on on the author
+            that was aselected. Hint: use an attribute selector to find
+            those articles that match the value, and then fade them in.
+        */
+    } else {
+      $('article').not('.template').not;
+    /* Otherwise, we should:
+        1. Show all the articles except the template */
+    }
+    $('#category-filter').val('');
+  });
 };
+
+
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
@@ -67,3 +86,8 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+articleView.setTeasers();
