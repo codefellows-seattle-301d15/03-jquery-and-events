9127d8e3f412190c7a105e83162e6bf28872998d
devonhackley <devonhackley@gmail.com>
2016-11-30 17:54:01 -0800

    what extra change

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -54,7 +54,7 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
+    /* TODONE:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
